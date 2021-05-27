8df591aa4dea4f3278db4a8a41750e58a15d529c
Anders Thomsen <anders.thomsen@LEGO.com>
2021-05-27 15:27:56 +0200

    Add chakra

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+﻿import * as React from "react"
+import { ChakraProvider } from "@chakra-ui/react"
+
+function App({ Component }) {
+    // 2. Use at the root of your app
+    return (
+        <ChakraProvider>
+            <Component />
+        </ChakraProvider>
+    )
+}
